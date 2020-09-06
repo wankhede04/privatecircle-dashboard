88ad97554a5181e48290e30228a91666ce0f8d0b
wankhede04 <wankhedevijay04@gmail.com>
2020-09-06 16:22:56 +0530

    Added material component.

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  MatFormFieldModule,
+  MatInputModule
+} from '@angular/material';
+import { MatTableModule } from '@angular/material/table';
+import { TableComponent } from '../table/table.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TableComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  exports: [TableComponent]
 })
 export class MaterialModule { }
