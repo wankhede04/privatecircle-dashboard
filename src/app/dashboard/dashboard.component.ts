d40254cda4f5606db840476de4faf32c59445e1b
wankhede04 <wankhedevijay04@gmail.com>
2020-09-08 19:52:52 +0530

    Added dashboard to assemble all the components.

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public getSearchKeyword(keyword: string) {
+    console.log(keyword)
+  }
+}
