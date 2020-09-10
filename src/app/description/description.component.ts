93230d375a3d5a9b8db3aecb8cab819f8a97cbe4
wankhede04 <wankhedevijay04@gmail.com>
2020-09-10 10:01:17 +0530

    Get industry details from parent.

--- src/app/description/description.component.ts
+++ src/app/description/description.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-description',
   templateUrl: './description.component.html',
   styleUrls: ['./description.component.scss']
 })
-export class DescriptionComponent implements OnInit {
+export class DescriptionComponent implements OnInit{
+  @Input()
+  public industryDetails: Array<string>;
 
   constructor() { }
 
