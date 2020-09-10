89a14e4de458a9eb77e430bba6c581a4b0a6bfb8
wankhede04 <wankhedevijay04@gmail.com>
2020-09-10 10:03:06 +0530

    Implemented the changes for showing the table with dynamic data.

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,45 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
 import { MatTableDataSource } from '@angular/material';
-
-export interface PeriodicElement {
-  listName: string;
-  date: string;
-  entities: number;
-  actions: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {date: '1 Jul', listName: 'Hydrogen Lelo, lavda lelo..', entities: 1, actions: 'H'},
-  {date: '2 Jul', listName: 'Helium', entities: 4, actions: 'He'},
-  {date: '3 Jul', listName: 'Lithium', entities: 6, actions: 'Li'},
-  {date: '4 Jul', listName: 'Beryllium', entities: 9, actions: 'Be'},
-  {date: '5 Jul', listName: 'Boron', entities: 811, actions: 'B'},
-  {date: '6 Jul', listName: 'Carbon', entities: 12, actions: 'C'},
-  {date: '7 Jul', listName: 'Nitrogen', entities: 14, actions: 'N'},
-  {date: '8 Jul', listName: 'Oxygen', entities: 994, actions: 'O'},
-  {date: '9 Jul', listName: 'Fluorine', entities: 18, actions: 'F'},
-  {date: '10 Jul', listName: 'Neon', entities: 20, actions: 'Ne'},
-  {date: '10 Jul', listName: 'Neon', entities: 20, actions: 'Ne'},
-  {date: '10 Jul', listName: 'Neon', entities: 20, actions: 'Ne'},
-  {date: '10 Jul', listName: 'Neon', entities: 20, actions: 'Ne'},
-  {date: '10 Jul', listName: 'Neon', entities: 20, actions: 'Ne'},
-];
+import { PeriodicElement } from '../models/table.model';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent implements OnInit {
+export class TableComponent implements OnInit, OnChanges {
   public displayedColumns: string[] = ['date', 'listName', 'entities', 'actions', 'details'];
-  public dataSource = new MatTableDataSource(ELEMENT_DATA);
 
-  constructor() { }
+  @Input()
+  public searchString: string;
+
+  @Input()
+  public tableData: PeriodicElement[];
+
+  @Output()
+  public detailsEvent: EventEmitter<Array<string>> = new EventEmitter();
+
+  public dataSource: MatTableDataSource<PeriodicElement>
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  ngOnChanges() {
+    this.dataSource = new MatTableDataSource(this.tableData);
+    if (this.searchString) {
+      this.dataSource.filter = this.searchString.trim().toLowerCase();
+    }
+  }
+
   public applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  public showIndustryDetails(details: Array<string>) {
+    this.detailsEvent.emit(details)
   }
 }
